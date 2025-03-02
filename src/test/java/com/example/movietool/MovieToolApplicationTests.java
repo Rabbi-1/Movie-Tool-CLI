0456da94278e51ed8313e1d80740db2ab969a227
Fuad Rabbi <fuadhrabbi@gmail.com>
2025-03-02 10:58:10 -0500

    Initial commit

---
+++ src/test/java/com/example/movietool/MovieToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movietool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
